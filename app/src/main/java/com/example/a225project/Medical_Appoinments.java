f5d4ae3f63265c709cabf01d538962d476102c69
Madhushanka00 <e19226@eng.pdn.ac.lk>
2023-06-28 12:59:04 +0530

    recycle view on appointment activity

--- app/src/main/java/com/example/a225project/Medical_Appoinments.java
+++ app/src/main/java/com/example/a225project/Medical_Appoinments.java
@@ -14,13 +14,28 @@ import java.util.ArrayList;
 public class Medical_Appoinments extends AppCompatActivity {
 
     ArrayList<TodayAppointmentsModel> TodayAppointmentModels = new ArrayList<>();
+    ArrayList<TodayAppointmentsModel> TomorrowAppointmentModels = new ArrayList<>();
+    ArrayList<TodayAppointmentsModel> NextAppointmentModels = new ArrayList<>();
+    //Today Data
+    String[] DoctorNamesToday ={"Kavindu bambaragama","Pubuduu madushith",};
+    String[] DoctorTitleToday ={"Medical Specialist","Dental Sergen"};
+    String[] AppointmentTypesToday = {"Medical checkup","Dental checkup"};
+    String[] timePeriodsToday = {"10.00AM - 11.00AM","10.00AM - 11.00AM"};
+    int[] ProfilePicsToday ={R.drawable.dp4,R.drawable.dp5};
 
-    String[] DoctorNames ={"Kavindu bambaragama","Pubuduu madushith","Mahesha Madhushanka","Pasindu Rangana","Uthsata Wikramarachchi"};
-    String[] DoctorTitle ={"Medical Specialist","Dental Sergen","Medical Specialist","Radiologist","Heart Specialist"};
-    String[] AppointmentTypes = {"Medical checkup","Dental checkup","Medical checkup","X-ray test","Heart checkup"};
-    String[] timePeriods = {"10.00AM - 11.00AM","10.00AM - 11.00AM","10.00AM - 11.00AM","10.00AM - 11.00AM","10.00AM - 11.00AM"};
-    int[] ProfilePics ={R.drawable.dp1,R.drawable.dp2,R.drawable.dp3,R.drawable.dp4,R.drawable.dp5};
+    //Tomorrow data
+    String[] DoctorNamesTomorrow ={"Kavindu bambaragama","Pubuduu madushith","Mahesha Madhushanka"};
+    String[] DoctorTitleTomorrow ={"Medical Specialist","Dental Sergen","Medical Specialist"};
+    String[] AppointmentTypesTomorrow = {"Medical checkup","Dental checkup","Medical checkup"};
+    String[] timePeriodsTomorrow = {"10.00AM - 11.00AM","10.00AM - 11.00AM","10.00AM - 11.00AM"};
+    int[] ProfilePicsTomorrow ={R.drawable.dp3,R.drawable.dp4,R.drawable.dp5};
 
+    //next 2-5 days
+    String[] DoctorNamesNext ={"Kavindu bambaragama","Pubuduu madushith","Mahesha Madhushanka","Pasindu Rangana","Uthsata Wikramarachchi"};
+    String[] DoctorTitleNext  ={"Medical Specialist","Dental Sergen","Medical Specialist","Radiologist","Heart Specialist"};
+    String[] AppointmentTypesNext  = {"Medical checkup","Dental checkup","Medical checkup","X-ray test","Heart checkup"};
+    String[] timePeriodsNext = {"10.00AM - 11.00AM","10.00AM - 11.00AM","10.00AM - 11.00AM","10.00AM - 11.00AM","10.00AM - 11.00AM"};
+    int[] ProfilePicsNext ={R.drawable.dp1,R.drawable.dp2,R.drawable.dp3,R.drawable.dp4,R.drawable.dp5};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +44,14 @@ public class Medical_Appoinments extends AppCompatActivity {
         RecyclerView recyclerView2=findViewById(R.id.myRecyclerView2);
         RecyclerView recyclerView3=findViewById(R.id.myRecyclerView3);
         setUpTodayAppointmentModels();
-        TodayAppointmentsAdapter adapter =new TodayAppointmentsAdapter(this,TodayAppointmentModels);
-        recyclerView.setAdapter(adapter);
-        recyclerView2.setAdapter(adapter);
-        recyclerView3.setAdapter(adapter);
+        setUpTomorrowAppointmentModels();
+        setUpNextAppointmentModels();
+        TodayAppointmentsAdapter adapterToday =new TodayAppointmentsAdapter(this,TodayAppointmentModels);
+        TodayAppointmentsAdapter adapterTomorrow =new TodayAppointmentsAdapter(this,TomorrowAppointmentModels);
+        TodayAppointmentsAdapter adapterNext =new TodayAppointmentsAdapter(this,NextAppointmentModels);
+        recyclerView.setAdapter(adapterToday);
+        recyclerView2.setAdapter(adapterTomorrow);
+        recyclerView3.setAdapter(adapterNext);
         ImageButton goBack =findViewById(R.id.goback_appt);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView2.setLayoutManager(new LinearLayoutManager(this));
@@ -49,8 +68,20 @@ public class Medical_Appoinments extends AppCompatActivity {
 
 
     private  void setUpTodayAppointmentModels(){
-        for(int i=0; i<DoctorNames.length;i++){
-            TodayAppointmentModels.add(new TodayAppointmentsModel(AppointmentTypes[i],timePeriods[i],DoctorNames[i],DoctorTitle[i],ProfilePics[i]));
+        for(int i=0; i<DoctorNamesToday.length;i++){
+            TodayAppointmentModels.add(new TodayAppointmentsModel(AppointmentTypesNext[i],timePeriodsNext[i],DoctorNamesNext[i],DoctorTitleNext[i],ProfilePicsNext[i]));
+
+        }
+    }
+    private  void setUpTomorrowAppointmentModels(){
+        for(int i=0; i<DoctorNamesTomorrow.length;i++){
+            TomorrowAppointmentModels.add(new TodayAppointmentsModel(AppointmentTypesTomorrow[i],timePeriodsTomorrow[i],DoctorNamesTomorrow[i],DoctorTitleTomorrow[i],ProfilePicsTomorrow[i]));
+
+        }
+    }
+    private  void setUpNextAppointmentModels(){
+        for(int i=0; i<DoctorNamesNext.length;i++){
+            NextAppointmentModels.add(new TodayAppointmentsModel(AppointmentTypesNext[i],timePeriodsNext[i],DoctorNamesNext[i],DoctorTitleNext[i],ProfilePicsNext[i]));
 
         }
     }
