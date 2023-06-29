99608f343522d7137be99e8c97b73f84f64af19f
Madhushanka00 <e19226@eng.pdn.ac.lk>
2023-06-29 11:16:53 +0530

    Doctor Registration UI Done

--- app/src/main/java/com/example/a225project/doctorReg.java
+++ app/src/main/java/com/example/a225project/doctorReg.java
@@ -4,30 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class doctorReg extends AppCompatActivity {
 
     EditText text;
-    ImageView view;
+    ImageButton view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_reg);
 
-        text = findViewById(R.id.editTextText);
-        view = findViewById(R.id.imageViewd);
-
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String text1 = text.getText().toString();
-                System.out.println(text1);
-            }
-        });
+//        text = findViewById(R.id.regDoctorName);
+//        view = findViewById(R.id.regDoctorSubmit);
+//
+//        view.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                String text1 = text.getText().toString();
+//                System.out.println(text1);
+//            }
+//        });
 
 
 
