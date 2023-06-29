aa34d78dc7002e71e55a507e625eb9841d6c9261
DhinushikaAbrew <e19006@eng.pdn.ac.lk>
2023-06-29 19:56:14 +0530

    activities of nurse and prescription were added

---
+++ app/src/main/java/com/example/a225project/prescriptiomDetails.java
@@ -0,0 +1,14 @@
+package com.example.a225project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class prescriptiomDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prescriptiom_details);
+    }
+}
