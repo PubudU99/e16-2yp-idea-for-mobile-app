79811fd580d638814934fb2fbd7695ed1f61c82e
DhinushikaAbrew <e19006@eng.pdn.ac.lk>
2023-07-01 03:16:03 +0530

    Ward details and ward inside details were added.

---
+++ app/src/main/java/com/example/a225project/admin_ward_details.java
@@ -0,0 +1,14 @@
+package com.example.a225project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class admin_ward_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_ward_details);
+    }
+}
