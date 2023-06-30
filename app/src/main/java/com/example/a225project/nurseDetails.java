a9bc5cdd6ce07f7eda1d12cf36bde2fe04c4d355
Bambara123 <115924758+Bambara123@users.noreply.github.com>
2023-07-01 02:04:11 +0530

    deatails pages added

---
+++ app/src/main/java/com/example/a225project/nurseDetails.java
@@ -0,0 +1,14 @@
+package com.example.a225project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nurseDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nurse_details);
+    }
+}
